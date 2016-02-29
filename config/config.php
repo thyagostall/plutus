089101f515bce77d4e2cd7223d6132e3826229aa
thyagostall <thstall@gmail.com>
2016-02-29 20:17:09 -0300

    Updated database url

--- config/config.php
+++ config/config.php
@@ -1,14 +1,18 @@
 <?php
 
 return [
+//    'db.options' => [
+//        'driver' => 'pdo_pgsql',
+//        'user' => 'some_user',
+//        'password' => 'some_password',
+//        'host' => 'localhost',
+//        'port' => 5432,
+//        'dbname' => 'some_db',
+//        'default_dbname' => 'postgres',
+//        'cache' => 'array'
+//    ],
     'db.options' => [
-        'driver' => 'pdo_pgsql',
-        'user' => 'thyago',
-        'password' => 'root',
-        'host' => 'localhost',
-        'port' => 5432,
-        'dbname' => 'plutus',
-        'default_dbname' => 'postgres',
+        'url' => getenv('DATABASE_URL'),
         'cache' => 'array'
     ]
 ];
